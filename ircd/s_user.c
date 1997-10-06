4f63a3d15dcb066abe955749ce22a737ac5c1a80
kalt <kalt>
1997-10-06 15:04:46 +0000

    services can now send notices to channels

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.28 1997/09/22 20:22:33 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.29 1997/09/23 19:12:44 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -996,7 +996,8 @@ int	parc, notice;
 		/*
 		** channel msg?
 		*/
-		if (IsPerson(sptr) && (chptr = find_channel(nick, NullChn)))
+		if ((IsPerson(sptr) || IsService(sptr)) &&
+		    (chptr = find_channel(nick, NullChn)))
 		    {
 			if (can_send(sptr, chptr) == 0)
 				sendto_channel_butone(cptr, sptr, chptr,
