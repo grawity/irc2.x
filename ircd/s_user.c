ee582a62955d590cf497d606eb230c7359400a96
kalt <kalt>
1998-02-07 15:37:57 +0000

    oper log is now more verbose

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.33 1998/01/07 20:44:34 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.34 1998/01/23 13:36:43 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -2241,9 +2241,11 @@ char	*parv[];
 		encr = "";
 #endif
 #if defined(USE_SYSLOG) && defined(SYSLOG_OPER)
-		syslog(LOG_INFO, "OPER (%s) (%s) by (%s!%s@%s)",
+		syslog(LOG_INFO, "OPER (%s) (%s) by (%s!%s@%s) [%s@%s]",
 			name, encr,
-			parv[0], sptr->user->username, sptr->sockhost);
+		       parv[0], sptr->user->username, sptr->user->host,
+		       sptr->auth, IsUnixSocket(sptr) ? sptr->sockhost :
+                       inetntoa((char *)&sptr->ip));
 #endif
 #ifdef FNAME_OPERLOG
 	      {
@@ -2262,9 +2264,11 @@ char	*parv[];
 		    (logfile = open(FNAME_OPERLOG, O_WRONLY|O_APPEND)) != -1)
 		{
 		  (void)alarm(0);
-			SPRINTF(buf, "%s OPER (%s) (%s) by (%s!%s@%s)\n",
-				    myctime(timeofday), name, encr, parv[0],
-				    sptr->user->username, sptr->sockhost);
+		  SPRINTF(buf, "%s OPER (%s) (%s) by (%s!%s@%s) [%s@%s]\n",
+			  myctime(timeofday), name, encr,
+			  parv[0], sptr->user->username, sptr->user->host,
+			  sptr->auth, IsUnixSocket(sptr) ? sptr->sockhost :
+			  inetntoa((char *)&sptr->ip));
 		  (void)alarm(3);
 		  (void)write(logfile, buf, strlen(buf));
 		  (void)alarm(0);
