904b7eb989cb26bd30edfb46046941f1f5e7b571
kalt <kalt>
1998-02-10 23:17:24 +0000

    broadcast restriction fix from Yegg

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.35 1998/02/07 15:37:57 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.37 1998/02/09 14:22:11 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -1051,7 +1051,8 @@ int	parc, notice;
 					   parv[0]), nick);
 				continue;
 			    }
-			if (strcasecmp(rindex(nick, '.'), s))
+			if ((s = (char *)rindex(ME, '.')) &&
+			    strcasecmp(rindex(nick, '.'), s))
 			    {
 				sendto_one(sptr, err_str(ERR_BADMASK,
 					   parv[0]), nick);
