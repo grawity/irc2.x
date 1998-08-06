ce103f9d32c3299f73bd2f449755bccc8e705a77
kalt <kalt>
1998-08-06 02:06:12 +0000

    notices to &AUTH are now triggered upon the slave request reduced memory usage by dynamically allocating the .inbuffer worked around the fd remapping problem by resending data when appropriate

--- iauth/iauth.c
+++ iauth/iauth.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: ircd.c,v 1.21 1998/06/12 23:35:19 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: iauth.c,v 1.1 1998/07/19 19:37:26 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -155,6 +155,13 @@ char	*argv[];
 		   );
 	init_io();
 	conf_read(NULL);
+
+#if defined(IAUTH_DEBUG)
+	if (debuglevel & ALOG_DIRCD)
+		sendto_ircd("1 G");
+	else
+#endif
+		sendto_ircd("0 G");
 
 	while (1)
 		loop_io();
