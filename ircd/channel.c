74f5991f2268e08976b06697607335656a9a9fae
kalt <kalt>
1999-01-16 22:10:51 +0000

    Insure++ reported missing return value

--- ircd/channel.c
+++ ircd/channel.c
@@ -32,7 +32,7 @@
  */
 
 #ifndef	lint
-static	char rcsid[] = "@(#)$Id: channel.c,v 1.83 1998/12/21 15:06:08 kalt Exp $";
+static	char rcsid[] = "@(#)$Id: channel.c,v 1.84 1998/12/29 20:14:15 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -2287,7 +2287,7 @@ char	*parv[];
 			    {
 				sendto_one(sptr, err_str(ERR_NOSUCHCHANNEL,
 							 parv[0]), parv[1]);
-				return;
+				return 0;
 			    }
 		    }
 		/* make sure user isn't already on channel */
