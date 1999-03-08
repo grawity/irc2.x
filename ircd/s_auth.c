c7d7c956d80aabdfff800b0724456e6b774657e0
kalt <kalt>
1999-03-08 19:25:18 +0000

    added FLAGS_DONEXAUTH to finish the work started yesterday.

--- ircd/s_auth.c
+++ ircd/s_auth.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_auth.c,v 1.28 1999/03/07 23:01:13 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_auth.c,v 1.29 1999/03/07 23:46:55 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -311,8 +311,11 @@ read_iauth()
 			    cptr->flags |= FLAGS_GOTID;
 			}
 		    else if (start[0] == 'D')
+		      {
 			    /*authentication finished*/
 			    ClearXAuth(cptr);
+			    SetDoneXAuth(cptr);
+		      }
 		    else
 			{
 			    /*
