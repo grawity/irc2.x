fe11333457c104f9a4e0da44360dffe701d78e2b
kalt <kalt>
1999-04-10 16:24:11 +0000

    moved debug notice (Q)

--- common/support.c
+++ common/support.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: support.c,v 1.10.2.3 1998/06/11 12:37:26 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: support.c,v 1.15 1998/12/13 00:02:33 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -536,10 +536,10 @@ char	*x;
 		dumpcore("MyFree %#x %ld %#x %#x", x, i, j,
 			 (k[3]<<24) | (k[2]<<16) | (k[1]<<8) | k[0]);
 
+	Debug((DEBUG_MALLOC, "MyFree(%#x)",x + SZ_CHST));
 #undef	free
 	(void)free(x);
 #define	free(x)	MyFree(x)
-	Debug((DEBUG_MALLOC, "MyFree(%#x)",x + SZ_CHST));
 
 	for (l = 0, s = marray; *s != x && l < mindex; l++, s++)
 		;
