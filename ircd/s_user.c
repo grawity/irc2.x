83547579f214d1ea938e41153ab58c59c0c944b2
kalt <kalt>
1999-06-17 01:16:40 +0000

    notice text update (iauth may not be running)

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.76 1999/06/07 21:19:59 kalt Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.77 1999/06/07 21:26:33 kalt Exp $";
 #endif
 
 #include "os.h"
@@ -402,7 +402,7 @@ char	*nick, *username;
 				if (timeofday - last > 300)
 				    {
 					sendto_flag(SCH_AUTH, 
-			    "iauth not running! (refusing new connections)");
+		    "iauth may not be running! (refusing new connections)");
 					last = timeofday;
 				    }
 				reason = "No iauth!";
