62f73cf6654216afd1fd216f9e1c6b9e1cdcccf5
jv <jv>
2002-03-24 19:56:33 +0000

    Send user modes to services with mask SERVICE_WANT_OPER.

--- ircd/s_service.c
+++ ircd/s_service.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_service.c,v 1.33 2001/12/29 20:54:08 q Exp $";
+static  char rcsid[] = "@(#)$Id: s_service.c,v 1.34 2002/01/06 05:55:25 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -195,7 +195,7 @@ int	wants;
 			   (wants & SERVICE_WANT_USER) ?
 			   ((wants & SERVICE_WANT_TOKEN) ?
 			    sptr->user->servp->tok : sptr->user->server) : ".",
-			   (wants & SERVICE_WANT_UMODE) ? umode : "+",
+			   (wants & SERVICE_WANT_UMODE | SERVICE_WANT_OPER) ? umode : "+",
 			   (wants & SERVICE_WANT_USER) ? sptr->info : "");
 	else
 		/* old style NICK + USER + UMODE */
