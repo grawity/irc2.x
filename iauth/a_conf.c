a1063d5f047d085ec560d31b7f43dcbadd9be7be
chopin <chopin>
2002-06-01 18:26:23 +0000

    Exit iauth on configuration error. Add note to iauth.conf about tabs.

--- iauth/a_conf.c
+++ iauth/a_conf.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: a_conf.c,v 1.21.2.5 2001/04/07 00:51:48 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: a_conf.c,v 1.22 2001/10/20 17:57:24 q Exp $";
 #endif
 
 #include "os.h"
@@ -41,10 +41,11 @@ u_int nb;
 char *msg, *chk;
 {
 	if (chk)
-		printf("line %d: %s\n", nb, msg);
+		printf("configuration error line %d: %s\n", nb, msg);
 	else
 		sendto_log(ALOG_IRCD|ALOG_DCONF, LOG_ERR,
 			   "Configuration error line %d: %s", nb, msg);
+	exit(0);
 }
 
 /*
