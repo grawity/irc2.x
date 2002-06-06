8ab1faf044ea88798030295e5f60af7581211740
chopin <chopin>
2002-06-06 13:49:51 +0000

    cleanup fixes regarding changed sendto_flog (also added missing exitc_ reasons for LOG_OLDFORMAT)

--- ircd/s_misc.c
+++ ircd/s_misc.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_misc.c,v 1.46 2002/06/02 00:47:07 jv Exp $";
+static  char rcsid[] = "@(#)$Id: s_misc.c,v 1.47 2002/06/06 04:59:27 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -476,7 +476,7 @@ char	*comment;	/* Reason for the exit */
 		    {
 # if defined(FNAME_CONNLOG) || defined(USE_SERVICES) || \
 	(defined(USE_SYSLOG) && defined(SYSLOG_CONN))
-			if (sptr->exitc == NULL || sptr->exitc == EXITC_REG)
+			if (sptr->exitc == '\0' || sptr->exitc == EXITC_REG)
 			{
 				sptr->exitc = EXITC_UNDEF;
 			}
