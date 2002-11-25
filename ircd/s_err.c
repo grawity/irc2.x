30444d7db0efbdce9f8d0c748234540237ac2de9
jv <jv>
2002-11-25 21:11:15 +0000

    RPL_YOURID: Let the UID be first, then text.

--- ircd/s_err.c
+++ ircd/s_err.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_err.c,v 1.37 2002/11/11 18:59:08 jv Exp $";
+static  char rcsid[] = "@(#)$Id: s_err.c,v 1.38 2002/11/22 21:19:26 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -70,7 +70,7 @@ char *	replies[] = {
 /* 039 */ (char *)NULL,
 /* 040 */ (char *)NULL,
 /* 041 */ (char *)NULL,
-/* 042 RPL_YOURID */	":%s 042 %s :Your unique ID is %s.",
+/* 042 RPL_YOURID */	":%s 042 %s %s :your unique ID",
 /* 043 RPL_SAVENICK */	":%s 043 %s :Nickname collision, forcing nick change to your unique ID.",
 /* 044 */ (char *)NULL,
 /* 045 */ (char *)NULL,
