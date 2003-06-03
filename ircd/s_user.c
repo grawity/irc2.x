11171cf72640ce1261b136e646cddf61dd131cee
chopin <chopin>
2003-06-03 22:06:34 +0000

    bad param count NICK coming from server must be dealt with

--- ircd/s_user.c
+++ ircd/s_user.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_user.c,v 1.148 2003/02/16 03:54:33 jv Exp $";
+static  char rcsid[] = "@(#)$Id: s_user.c,v 1.149 2003/02/19 13:27:37 jv Exp $";
 #endif
 
 #include "os.h"
@@ -1060,6 +1060,12 @@ nickkilldone:
 	if (IsServer(sptr))
 	    {
 		char	*pv[7];
+
+		if (parc != 8)
+		{
+			/* New NICK *must* have proper param count */
+			goto badparamcountkills;
+		}
 
 		/* A server introducing a new client, change source */
 		sptr = make_client(cptr);
