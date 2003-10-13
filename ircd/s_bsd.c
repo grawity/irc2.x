71ef22fb000c94af3c799e69b654bf22d06955d9
chopin <chopin>
2003-10-13 23:58:23 +0000

    remove confusing "udp port recvfrom()" message.

--- ircd/s_bsd.c
+++ ircd/s_bsd.c
@@ -35,7 +35,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_bsd.c,v 1.98 2003/08/12 16:56:49 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: s_bsd.c,v 1.99 2003/10/13 21:48:53 q Exp $";
 #endif
 
 #include "os.h"
@@ -3047,6 +3047,8 @@ static	void	polludp()
 			return;
 		else
 		    {
+#if 0
+/* seems to create more confusion than it's worth */
 			char buf[100];
 
 			sprintf(buf, "udp port recvfrom() from %s to %%s: %%s",
@@ -3064,6 +3066,7 @@ static	void	polludp()
 #endif
 				);
 			report_error(buf, &me);
+#endif /* confusion */
 			return;
 		    }
 	    }
