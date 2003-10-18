cd558df0fe6bf758ef2a460c972de8fb6dc6f99c
q <q>
2003-10-18 19:48:21 +0000

    Fix warnings.

--- common/send.c
+++ common/send.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: send.c,v 1.64 2003/10/14 20:35:15 q Exp $";
+static  char rcsid[] = "@(#)$Id: send.c,v 1.65 2003/10/18 15:31:28 q Exp $";
 #endif
 
 #include "os.h"
@@ -400,9 +400,9 @@ static	aClient	anon = { NULL, NULL, NULL, &ausr, NULL, NULL, 0, 0,/*flags*/
 # endif
 			 0, {0, 0, NULL }, {0, 0, NULL },
 			 0, 0, 0L, 0L, 0, 0, 0, NULL, NULL, 0, NULL, 0
-# if defined(__STDC__)	/* hack around union{} initialization	-Vesa */
+//# if defined(__STDC__)	/* hack around union{} initialization	-Vesa */
 			 , {0}, NULL, "", "", EXITC_UNDEF
-# endif
+//# endif
 			};
 #endif
 
