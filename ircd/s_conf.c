b4b79fa8f00508a6785a523e8a5f219d99045747
chopin <chopin>
2004-03-14 17:46:00 +0000

    remove CACHED_MOTD, it's now default and cannot be turned off (why would anyone want different behaviour?) also fix tiny bug of always rereading motd, even if it did not change (contrary to what docs said)

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -48,7 +48,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: s_conf.c,v 1.95 2004/03/05 22:06:10 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: s_conf.c,v 1.97 2004/03/07 02:47:51 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -1011,9 +1011,7 @@ int	rehash(aClient *cptr, aClient *sptr, int sig)
 			free_conf(tmp2);
 		}
 	
-#ifdef CACHED_MOTD
 	read_motd(IRCDMOTD_PATH);
-#endif
 	if (rehashed == 1)
 	{
 		/* queue another rehash for later */
