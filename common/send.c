ff668a85ecd0dc755de562dbcde5bb3ee54fad1f
chopin <chopin>
2004-03-18 00:54:46 +0000

    guard all unix socket uses by #ifdef UNIXPORT

--- common/send.c
+++ common/send.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: send.c,v 1.67 2003/10/18 20:11:39 q Exp $";
+static  char rcsid[] = "@(#)$Id: send.c,v 1.68 2004/03/07 02:47:50 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -468,9 +468,11 @@ static	int	vsendpreprep(aClient *to, aClient *from, char *pattern, va_list va)
 			if (!flag && MyConnect(from) && *user->host)
 			    {
 				(void)strcat(psendbuf, "@");
+#ifdef UNIXPORT
 				if (IsUnixSocket(from))
 				    (void)strcat(psendbuf, user->host);
 				else
+#endif
 				    (void)strcat(psendbuf, from->sockhost);
 			    }
 		    }
