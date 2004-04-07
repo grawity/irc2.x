a8e3d8feb51d375fc05c004637d711ed143dc3f2
chopin <chopin>
2004-04-07 17:02:38 +0000

    changed IsListening to IsListener

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: ircd.c,v 1.125 2004/03/21 18:24:38 jv Exp $";
+static  char rcsid[] = "@(#)$Id: ircd.c,v 1.126 2004/04/07 16:56:59 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -916,7 +916,7 @@ int	main(int argc, char *argv[])
                     {   
 			if (!(acptr = local[i]))
 				continue;
-			if (IsListening(acptr))
+			if (IsListener(acptr))
 				break;
 			acptr = NULL;
 		    }
