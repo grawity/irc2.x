849303fa5017a8d33c77f81fba2f79c9d88efc2f
jv <jv>
2004-08-02 15:47:11 +0000

    main(), setup_me(): Call register_server();

--- ircd/ircd.c
+++ ircd/ircd.c
@@ -19,7 +19,7 @@
  */
 
 #ifndef lint
-static  char rcsid[] = "@(#)$Id: ircd.c,v 1.137 2004/06/19 18:05:46 chopin Exp $";
+static  char rcsid[] = "@(#)$Id: ircd.c,v 1.138 2004/07/11 18:38:16 jv Exp $";
 #endif
 
 #include "os.h"
@@ -742,6 +742,7 @@ int	main(int argc, char *argv[])
 	bzero((char *)&me, sizeof(me));
 
 	make_server(&me);
+	register_server(&me);
 
 	version = make_version();	/* Generate readable version string */
 
@@ -1014,6 +1015,7 @@ int	main(int argc, char *argv[])
 
 		tmp = make_client(NULL);
 		make_server(tmp);
+		register_server(tmp);
 
 		tmp->fd = 0;
 		tmp->flags = FLAGS_LISTEN;
