e498974ce5866d835002244cf5570ab4c010f827
chopin <chopin>
2004-10-27 13:21:34 +0000

    return NOMATCH for null masks too.

--- common/match.c
+++ common/match.c
@@ -18,7 +18,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: match.c,v 1.13 2004/06/11 18:00:59 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: match.c,v 1.14 2004/10/01 20:22:12 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -178,7 +178,7 @@ int	match(char *mask, char *name)
 	char	*ma = mask, *na = name;
 	int	wild = 0, q = 0, calls = 0;
 
-	if (!*mask)
+	if (BadPtr(mask) || BadPtr(name))
 		return 1;
 
 	if (mask[0]=='*' && mask[1]=='\0')
