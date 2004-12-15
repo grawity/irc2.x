69b280f58e7e81fa4b852f5cbb23f98e15f1754e
chopin <chopin>
2004-12-15 01:25:31 +0000

    check split after doing SET SPLIT

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: s_serv.c,v 1.256 2004/11/21 00:41:16 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: s_serv.c,v 1.257 2004/12/12 19:26:11 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -3433,6 +3433,7 @@ int	m_set(aClient *cptr, aClient *sptr, int parc, char *parv[])
 						iconf.split_minusers, tmp);
 					iconf.split_minusers = tmp;
 				}
+				check_split();
 				break;
 			}
 		} /* switch(acmd) */
