49ac1848d796c6c61d3375094baed0a5f9f344c1
chopin <chopin>
2005-02-15 18:37:36 +0000

    going back to fdopen

--- ircd/s_conf.c
+++ ircd/s_conf.c
@@ -48,7 +48,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: s_conf.c,v 1.154 2005/01/30 17:22:12 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: s_conf.c,v 1.155 2005/02/12 01:34:15 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -1396,9 +1396,7 @@ int 	initconf(int opt)
 		return -1;
 	    }
 #if defined(CONFIG_DIRECTIVE_INCLUDE)
-	/* Note for a future: do not use fdopen() for its 256 fd limit --B. */
-	close(fd);
-	fdn = fopen(configfile, "r");
+	fdn = fdopen(fd, "r");
 	if (fdn == NULL)
 	{
 		if (serverbooting)
