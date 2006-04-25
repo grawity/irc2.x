69b1232624cd2dab51de865f936dacbe0a4c9cba
chopin <chopin>
2006-04-25 22:15:54 +0000

    after few days (or even hours) there is no need to report given link failure to the oper that connected it

--- ircd/s_serv.c
+++ ircd/s_serv.c
@@ -22,7 +22,7 @@
  */
 
 #ifndef lint
-static const volatile char rcsid[] = "@(#)$Id: s_serv.c,v 1.275 2005/06/01 16:27:24 chopin Exp $";
+static const volatile char rcsid[] = "@(#)$Id: s_serv.c,v 1.276 2005/09/02 13:59:53 chopin Exp $";
 #endif
 
 #include "os.h"
@@ -3038,6 +3038,11 @@ int	m_eob(aClient *cptr, aClient *sptr, int parc, char *parv[])
 int	m_eoback(aClient *cptr, aClient *sptr, int parc, char *parv[])
 {
 	cptr->flags &= ~FLAGS_CBURST;
+	if (cptr->serv && cptr->serv->byuid[0])
+	{
+		/* no need to report link break few days later, is there? --B. */
+		cptr->serv->byuid[0] = '\0';
+	}
 	return 0;
 }
 
